1496754e03f36beb9ca6c32a847d101e68da30f7
Sean Lindo <Sean@Seans-MacBook-Pro.local>
2014-10-23 13:15:53 -0400

    Fixing a syntax eerror and accounting for changes made in new linux kernel

--- Vagrantfile
+++ Vagrantfile
@@ -5,10 +5,10 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.network "forwarded_port", guest: 80, host: 8080
 
   config.vm.provision :shell, :path => "provision/shell/provision_prereqs.sh"
-  
+
   config.vm.provision "puppet" do |puppet|
     puppet.manifests_path = "provision/puppet/manifests"
     puppet.manifest_file = "main.pp"
-    puppet.module_path = ["provision/puppet/public_modules", provision/puppet/private_modules]
+    puppet.module_path = ["provision/puppet/public_modules", "provision/puppet/private_modules"]
   end
 end
--- provision/shell/provision_prereqs.sh
+++ provision/shell/provision_prereqs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
- 
+
 # Directory in which librarian-puppet should manage its modules directory
 PUPPET_DIR=/vagrant/provision/puppet
- 
+
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
- 
+
 $(which git > /dev/null 2>&1)
 FOUND_GIT=$?
 if [ "$FOUND_GIT" -ne '0' ]; then
@@ -14,7 +14,7 @@ if [ "$FOUND_GIT" -ne '0' ]; then
   FOUND_APT=$?
   $(which yum > /dev/null 2>&1)
   FOUND_YUM=$?
- 
+
   if [ "${FOUND_YUM}" -eq '0' ]; then
     yum -q -y makecache
     yum -q -y install git
@@ -29,12 +29,23 @@ if [ "$FOUND_GIT" -ne '0' ]; then
 else
   echo 'git found.'
 fi
- 
-if [ "$(gem search -i librarian-puppet)" = "false" ]; then
+
+$(which gem > /dev/null 2>&1)
+FOUND_GEM=$?
+if [ "$FOUND_GEM" -ne '0' ]; then
   apt-get install -y build-essential
   apt-get install -y ruby-dev
+fi
+
+$(which puppet > /dev/null 2>&1)
+FOUND_PUPPET=$?
+if [ "$FOUND_PUPPET" -ne '0' ]; then
+  apt-get install -y puppet-common
+fi
+
+if [ "$(gem search -i librarian-puppet)" = "false" ]; then
   gem install librarian-puppet
-   cd $PUPPET_DIR && librarian-puppet install --path public_modules 
+   cd $PUPPET_DIR && librarian-puppet install --path public_modules
 else
   cd $PUPPET_DIR && librarian-puppet update
 fi
